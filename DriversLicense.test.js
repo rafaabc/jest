2ce611233d8ba3b2178e524aae3e5ae3a31f479c
rafaabc <faelsabc21@gmail.com>
2022-01-21 08:31:48 -0300

    change the matchers to fit better the string assertions

--- DriversLicense.test.js
+++ DriversLicense.test.js
@@ -5,22 +5,22 @@ describe("Should test the availabiliy to drive based on the age and a valid driv
   });
 
   test("Should NOT be able to drive with the age of 17", () => {
-    expect(isAbleToDrive(17, false)).toBe("You are NOT able to drive");
+    expect(isAbleToDrive(17, false)).toMatch(/You are NOT able to drive/);
   });
 
   test("Should NOT be able to drive with the age of 18 and no drivers License", () => {
-    expect(isAbleToDrive(18, false)).toBe("You are NOT able to drive");
+    expect(isAbleToDrive(18, false)).toMatch(/You are NOT able to drive/);
   });
 
   test("Should be able to drive with the age of 18 and with drivers License", () => {
-    expect(isAbleToDrive(18, true)).toBe("You are able to drive");
+    expect(isAbleToDrive(18, true)).toMatch(/You are able to drive/);
   });
 
   test("Should NOT be able to drive with the age of 19 and no drivers license", () => {
-    expect(isAbleToDrive(19, false)).toBe("You are NOT able to drive");
+    expect(isAbleToDrive(19, false)).toMatch(/You are NOT able to drive/);
   });
 
   test("Should be able to drive with the age of 19 and with drivers License", () => {
-    expect(isAbleToDrive(19, true)).toBe("You are able to drive");
+    expect(isAbleToDrive(19, true)).toMatch(/You are able to drive/);
   });
 });
